7001b4fef67be38bd9130347f59efe1f79df1c3d
Greg <gjmooney@asu.edu>
2023-08-29 12:51:50 +0200

    Fix colors for dark mode

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,27 +7,29 @@ import Link from "next/link";
 
 const Logo = ({}) => {
   return (
-    <motion.div
-      className="mt-2 rounded-full bg-primary"
-      whileHover={{
-        backgroundColor: [
-          "#0F172A",
-          "#F7D8B9",
-          "#AEC9E0",
-          "#F2B2A8",
-          "#D9D9D9",
-          "#0F172A",
-        ],
-        transition: { duration: 3, repeat: Infinity },
-      }}
-    >
-      <Link
-        href="/"
-        className="flex h-16 w-16 items-center justify-center text-2xl font-bold text-primary-foreground"
+    <>
+      <motion.div
+        className="mt-2 rounded-full border border-transparent bg-[#0F172A] dark:border-primary"
+        whileHover={{
+          backgroundColor: [
+            "#0F172A",
+            "#F7D8B9",
+            "#AEC9E0",
+            "#F2B2A8",
+            "#D9D9D9",
+            "#0F172A",
+          ],
+          transition: { duration: 3, repeat: Infinity },
+        }}
       >
-        GM
-      </Link>
-    </motion.div>
+        <Link
+          href="/"
+          className="flex h-16 w-16 items-center justify-center text-2xl font-bold text-[#F8FAFC] text-primary"
+        >
+          GM
+        </Link>
+      </motion.div>
+    </>
   );
 };
 
