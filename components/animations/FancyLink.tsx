4a4aebfe89324b5814acdd81e62414112bc15a0d
Greg <gjmooney@asu.edu>
2023-08-31 14:46:52 +0200

    Refactor FancyLink to work with Shad Sheet

--- components/animations/FancyLink.tsx
+++ components/animations/FancyLink.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 
 interface FancyLinkProps {
   href: string;
@@ -11,25 +12,30 @@ interface FancyLinkProps {
   mobile?: boolean;
 }
 
-const FancyLink = ({
-  href,
-  title,
-  className = "",
-  mobile = false,
-}: FancyLinkProps) => {
-  const pathname = usePathname();
-  return (
-    <Link href={href} className={`${className} group relative`}>
-      {title}
-      <span
-        className={cn(
-          "absolute -bottom-0.5 left-0 h-[1px] bg-primary duration-300 ease-in-out group-hover:w-full",
-          pathname === href ? "h-[1.5px] w-full" : "w-0",
-          mobile ? "bg-secondary" : "bg-primary",
-        )}
-      />
-    </Link>
-  );
-};
+const FancyLink = React.forwardRef<HTMLAnchorElement, FancyLinkProps>(
+  (props, ref) => {
+    const { href, title, className = "", mobile = false, ...other } = props;
+    const pathname = usePathname();
+    return (
+      <Link
+        {...other}
+        ref={ref}
+        href={href}
+        className={`${className} group relative`}
+      >
+        {title}
+        <span
+          className={cn(
+            "absolute -bottom-0.5 left-0 h-[1px] bg-primary duration-300 ease-in-out group-hover:w-full",
+            pathname === href ? "h-[1.5px] w-full" : "w-0",
+            mobile ? "bg-secondary" : "bg-primary",
+          )}
+        />
+      </Link>
+    );
+  },
+);
+
+FancyLink.displayName = "FancyLink";
 
 export default FancyLink;
