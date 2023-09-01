b0f84e45a947444976c915daa06435d16b395f08
Greg <gjmooney@asu.edu>
2023-09-01 14:02:41 +0200

    Fix article animation:

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -108,18 +108,24 @@ const FeaturedArticle = ({
 
 const Article = ({ img, title, time, link }: FeaturedArticleProps) => {
   return (
-    <motion.li
+    <motion.div
       initial={{ y: 200, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
+      whileHover={{ scale: 1.05 }}
       viewport={{ once: true }}
-      transition={{ duration: 0.5, ease: "easeIn" }}
-      className="relative my-4 flex w-full flex-col justify-between rounded-xl border border-b-4 border-r-4 border-primary p-4 py-6 transition first:mt-0 last:mb-0 md:flex-row"
+      transition={{ duration: 0.8, ease: "easeInOut" }}
+      className="my-4"
     >
-      <MovingImage title={title} link={link} img={img} />
-      <span className="text-sm font-semibold text-pastel-purple md:pl-4 md:text-base">
-        {time}
-      </span>
-    </motion.li>
+      <motion.li className="relative flex w-full flex-col justify-between rounded-xl border border-b-4 border-r-4 border-primary p-4 py-6 transition first:mt-0 last:mb-0 md:flex-row">
+        {/* <MovingImage title={title} link={link} img={img} /> */}
+        <h2 className="text-xl font-semibold capitalize hover:underline">
+          {title}
+        </h2>
+        <span className="text-sm font-semibold text-pastel-purple md:pl-4 md:text-base">
+          {time}
+        </span>
+      </motion.li>
+    </motion.div>
   );
 };
 
